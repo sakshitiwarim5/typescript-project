cc8c43c1b5825baed73223e132d4ec0cbcaeedf9
Sakshi Tiwari <sakshitiwarim5@gmail.com>
2025-04-22 22:53:43 +0530

    lern

--- src/a.ts
+++ src/a.ts
@@ -15,4 +15,4 @@ const greet = (name: string) => 'Hello, ${name}!';
 
 // Zod is for runtime type checks (99%)
 
-// Zod is a TypeScript-first schema declaration and validation library. It allows you to define schemas for your data and validate them at runtime. This is useful for ensuring that the data you receive from external sources (like APIs) matches the expected structure.
+// Zod is a TypeScript-first schema declaration and validation library. It allows you to define schemas for your data and validate them at runtime. This is useful for ensuring that the data you receive from external sources (like APIs) matches the expected structure.//zeptoo
